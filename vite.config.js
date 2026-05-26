f24332805e3cd783404832763a8f4293e13b2269
jbrownsberger <118187976+jbrownsberger@users.noreply.github.com>
2026-05-26 09:52:27 -0400

    Add @vitejs/plugin-react to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,8 @@
-import { defineConfig } from 'vite'
-export default defineConfig({ base: './', build: { outDir: 'dist' } })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: { outDir: 'dist' }
+});
